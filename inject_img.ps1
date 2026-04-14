772d643429c46d99700f31ae33bac597b8bb8c6d
okprabir <prabirgiri2005@gmail.com>
2026-04-14 18:41:37 +0530

    Delete inject_img.ps1

--- inject_img.ps1
+++
@@ -1,9 +0,0 @@
-$imgBytes = [System.IO.File]::ReadAllBytes("d:\chinu bhai website\assets\instructor_profile.png")
-$base64 = [System.Convert]::ToBase64String($imgBytes)
-$htmlFile = "d:\chinu bhai website\index.html"
-$content = Get-Content $htmlFile -Raw
-$newSrc = "data:image/png;base64,$base64"
-$pattern = 'src="assets/instructor_profile\.png[^"]*"'
-$replacement = "src=`"$newSrc`""
-$content -replace $pattern, $replacement | Set-Content $htmlFile -Encoding UTF8
-Write-Host "Image successfully embedded as Base64 in index.html"
