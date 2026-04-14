c412bbca492968cabeae63281a15cdbd1584c681
okprabir <prabirgiri2005@gmail.com>
2026-04-14 18:42:12 +0530

    Delete revert_img.ps1

--- revert_img.ps1
+++
@@ -1,6 +0,0 @@
-$htmlFile = "d:\chinu bhai website\index.html"
-$content = Get-Content $htmlFile -Raw
-$pattern = '<img src="data:image/png;base64,[^"]*"'
-$replacement = '<img src="assets/instructor_profile.png?v=3"'
-$content -replace $pattern, $replacement | Set-Content $htmlFile -Encoding UTF8
-Write-Host "Image successfully reverted to external file path in index.html"
